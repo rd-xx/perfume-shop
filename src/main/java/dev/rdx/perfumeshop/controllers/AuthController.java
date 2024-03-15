3dafd0d8f73aa2bd6865a7c42f16b3a9f72504b1
rdx <reydux95@gmail.com>
2024-03-15 09:52:45 +0100

    fix(auth): remove controller prefix

--- src/main/java/dev/rdx/perfumeshop/controllers/AuthController.java
+++ src/main/java/dev/rdx/perfumeshop/controllers/AuthController.java
@@ -3,14 +3,14 @@ package dev.rdx.perfumeshop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/auth")
+@Controller
 public class AuthController {
-    @GetMapping("/sign-in")
+    @GetMapping("/auth/sign-in")
     public String signIn() {
         return "auth/sign-in";
     }
 
-    @GetMapping("/sign-up")
+    @GetMapping("/auth/sign-up")
     public String signUp() {
         return "auth/sign-up";
     }
